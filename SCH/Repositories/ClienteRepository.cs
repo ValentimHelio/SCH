3dbc7a2193742c0831b2a4c5229c32bba4336e8a
Valentim Helio de Oliveira <timvale@hotmail.com>
2025-02-03 09:27:34 -0400

    Alterações

---
+++ SCH/Repositories/ClienteRepository.cs
@@ -0,0 +1,15 @@
+﻿using SCH.Context;
+using SCH.Models;
+using SCH.Repositories.Interfaces;
+
+namespace SCH.Repositories
+{
+    public class ClienteRepository : Repository<Cliente>, IClienteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClienteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
